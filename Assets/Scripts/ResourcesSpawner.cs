ade31dbc6bc5ce66eaecfcfe49e8fdccebd26c65
lumpn <lumpn@users.noreply.github.com>
2022-05-27 20:42:26 +0800

    Demonstrating Resources

--- Assets/Scripts/ResourcesSpawner.cs
+++ Assets/Scripts/ResourcesSpawner.cs
@@ -3,14 +3,14 @@ using UnityEngine;
 
 public sealed class ResourcesSpawner : MonoBehaviour
 {
-    [SerializeField] private string prefabName;
+  [SerializeField] private string prefabName;
 
-    protected IEnumerator Start()
-    {
-        var request = Resources.LoadAsync<GameObject>(prefabName);
-        yield return request;
+  protected IEnumerator Start()
+  {
+    var request = Resources.LoadAsync<GameObject>(prefabName);
+    yield return request;
 
-        var prefab = request.asset;
-        Object.Instantiate(prefab, transform.position, transform.rotation, transform.parent);
-    }
+    var prefab = request.asset;
+    Object.Instantiate(prefab, transform.position, transform.rotation, transform.parent);
+  }
 }
